52302a24810d9887f138379b42a8efbd159afc71
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2025-11-07 11:57:14 +0700

    memperbaiki semua error dan typo

--- app/src/main/java/com/example/navigasiku/view/Formulir.kt
+++ app/src/main/java/com/example/navigasiku/view/Formulir.kt
@@ -1,13 +1,20 @@
 package com.example.navigasiku.view
 
+import com.example.navigasiku.R
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.DividerDefaults.Thickness
 import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.graphics.Color
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -40,17 +47,18 @@ fun FormIsian(
                 .padding(all=20.dp)
                 .width(width=250.dp), thickness = Thickness, color=Color.Red)
             Row{
-                jenisK.forEach{
-                    item ->
-                    Row(verticalAlignment = Alignment.CenterVertically{
-                        RadioButton(
-                            selected = false,
-                            onClick = {item}
-                        )
-                        Text(text = item)
-                    })
+                jenisK.forEach {
+                        item->
+                        Row(verticalAlignment = Alignment.CenterVertically) {
+                            RadioButton(
+                                selected = false,
+                                onClick = { item }
+                            )
+                            Text(text = item)
+                        }
                 }
             }
+
             HorizontalDivider(modifier = Modifier
                 .padding(all=20.dp)
                 .width(width=250.dp),
