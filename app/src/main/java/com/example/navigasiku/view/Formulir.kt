bef25bc3bfec7df354124068838e2ef794722a34
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2025-11-07 09:49:42 +0700

    menambahkan fungsi composable FormIsian dengan parameter jenis kelamin dan tombol submit

---
+++ app/src/main/java/com/example/navigasiku/view/Formulir.kt
@@ -0,0 +1,10 @@
+package com.example.navigasiku.view
+
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormIsian(
+    jenisK:List<String> = listOf("Laki-laki", "Perempuan"),
+    OnSubmitBtnClick : () -> Unit
+){}
