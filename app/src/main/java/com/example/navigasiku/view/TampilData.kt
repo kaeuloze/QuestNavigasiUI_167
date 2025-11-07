d80ca191d0d11caad2cb2306b41bbf15bff8122d
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2025-11-07 13:08:30 +0700

    menambahkan tombol “Back” dan Spacer di bagian bawah tampilan

--- app/src/main/java/com/example/navigasiku/view/TampilData.kt
+++ app/src/main/java/com/example/navigasiku/view/TampilData.kt
@@ -61,7 +61,13 @@ fun TampilData(
                     }
                     HorizontalDivider(thickness = 1.dp, color = Color.Cyan)
                 }
-
+                Spacer(modifier = Modifier.height(10.dp))
+                Button(
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = onBackBtnClick
+                ) {
+                    Text(text = stringResource(id = R.string.back))
+                }
             }
         }
     }
