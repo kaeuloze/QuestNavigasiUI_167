e16077a556d3caedffb018f94573bd5679bb2e4c
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2025-11-07 10:16:02 +0700

    menambahkan RadioButton untuk pilihan jenis kelamin pada layout isiRuang

--- app/src/main/java/com/example/navigasiku/view/Formulir.kt
+++ app/src/main/java/com/example/navigasiku/view/Formulir.kt
@@ -39,7 +39,18 @@ fun FormIsian(
             HorizontalDivider(modifier = Modifier
                 .padding(all=20.dp)
                 .width(width=250.dp), thickness = Thickness, color=Color.Red)
-
+            Row{
+                jenisK.forEach{
+                    item ->
+                    Row(verticalAlignment = Alignment.CenterVertically{
+                        RadioButton(
+                            selected = false,
+                            onClick = {item}
+                        )
+                        Text(text = item)
+                    })
+                }
+            }
         }
     }
 }
